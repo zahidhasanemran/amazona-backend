103e7ff90a4bf10a9804dc186297ea42217dfce9
Emran <emranhstbq@gmail.com>
2020-10-30 00:19:55 +0600

    login function check

--- server.js
+++ server.js
@@ -1,54 +1,63 @@
-import express from 'express';
-import userRoute from './routes/userRoute'
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors"); 
 import data from './data'
 import config from './config'
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser'
+import userRoute from './routes/userRoute'
 
 dotenv.config();
-const mongodbUrl = config.MONGODB_URL;
+const mongodbUrl = config.MONGODB_URL || 'mongodb://localhost/amazona';
 
 const port = process.env.PORT || 5000;
-
-mongoose.connect(mongodbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
-.then(() => {
-    app.use("/api/users", userRoute);
-    
-})
-.catch(error => console.log(error.message))
-
-
 const app = express();
 app.use(bodyParser.json())
-// app.use("/api/users", userRoute);
+app.use(express.static('public'))
+var corsOptions = {
+    origin: "http://localhost:3000"
+};
+  
+app.use(cors(corsOptions));
+
+// connectDB();
+
+
+
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+
+
+
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
 app.get("/api/products/:id", (req, res) => {
     let id = req.params.id;
-    // console.log(id);
     let product = data.products.find(x => x.id == id);
     console.log(product);
     res.send(product);
 });
 
-
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to express application." });
-  });
+});
 
 
-app.listen(port, () => {console.log("server started at 5000");});
-app.use(express.static('public'));
 
+
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then((res) => {
+    console.log(res);
+    app.use("/api/users", userRoute);
+}).catch(error => console.log(error.message)); 
+app.listen(port, () => {console.log("server started at 5000")});
