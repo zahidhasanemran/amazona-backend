2ea8af7be0ae8161a7aeeccfab76055c8f1f6c23
Emran <emranhstbq@gmail.com>
2020-10-30 01:51:41 +0600

    kjdasklj

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions));
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://redux-ecomerce.herokuapp.com/"); 
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
